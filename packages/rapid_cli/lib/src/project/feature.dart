c6b0774e57e34a7237db52c5d164a877b756906f
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-01-04 16:23:49 +0100

    feat: add feature class

---
+++ packages/rapid_cli/lib/src/project/feature.dart
@@ -0,0 +1,144 @@
+import 'package:path/path.dart' as p;
+import 'package:rapid_cli/src/core/dart_package.dart';
+import 'package:rapid_cli/src/core/platform.dart';
+import 'package:rapid_cli/src/core/yaml_file.dart';
+import 'package:rapid_cli/src/project/platform_directory.dart';
+import 'package:universal_io/io.dart';
+
+// TODO this class has a diffrent abstraction level then all other project/filesystem related classes
+// in future either this class should have the same abstraction level than them or they should have the
+// same than this (tending to second) -> leads to major refactors in tests and commands
+
+/// Thrown when [Feature.findArbFileByLanguage] fails to find the arb file related to language on disk.
+class ArbFileNotExisting implements Exception {}
+
+/// {@template feature}
+/// Abstraction of a feature of a Rapid project.
+/// {@endtemplate}
+class Feature {
+  /// {@macro feature}
+  Feature({
+    required this.name,
+    required PlatformDirectory platformDirectory,
+  })  : _l10nFile = L10nFile(
+          path: p.join(
+            platformDirectory.path,
+            '${platformDirectory.project.melosFile.name()}_${platformDirectory.platform.name}_$name',
+          ),
+        ),
+        _package = DartPackage(
+          path: p.join(
+            platformDirectory.path,
+            '${platformDirectory.project.melosFile.name()}_${platformDirectory.platform.name}_$name',
+          ),
+        ),
+        _platformDirectory = platformDirectory;
+
+  final L10nFile _l10nFile;
+
+  /// The underlying package file.
+  final DartPackage _package;
+
+  final PlatformDirectory _platformDirectory;
+
+  Set<ArbFile> _arbFiles() {
+    final arbDir = Directory(
+      p.join(path, 'lib', 'src', 'presentation', 'l10n', 'arb'),
+    );
+
+    return arbDir
+        .listSync()
+        .whereType<File>()
+        .where((e) => e.path.endsWith('.arb'))
+        .map(
+          (e) => ArbFile(
+            language: e.path.split('.').first.split('_').last,
+            feature: this,
+          ),
+        )
+        .toSet();
+  }
+
+  void delete() => _package.delete();
+
+  String defaultLanguage() {
+    final l10nFile = _l10nFile;
+    final templateArbFile = l10nFile.templateArbFile();
+
+    return templateArbFile.split('.').first.split('_').last;
+  }
+
+  bool exists() => _package.exists();
+
+  ArbFile findArbFileByLanguage(String language) {
+    try {
+      Set arbFiles = _arbFiles();
+      return arbFiles.firstWhere((e) => e.language == language);
+    } catch (_) {
+      throw ArbFileNotExisting();
+    }
+  }
+
+  bool supportsLanguage(String language) =>
+      _arbFiles().map((e) => e.language).contains(language);
+
+  final String name;
+
+  String get path => _package.path;
+
+  Platform get platform => _platformDirectory.platform;
+}
+
+/// {@template arb_file}
+/// Abstraction of a arb file of a feature of a Rapid project.
+/// {@endtemplate}
+class ArbFile {
+  /// {@macro arb_file}
+  ArbFile({
+    required this.language,
+    required Feature feature,
+  }) : _file = File(
+          p.join(
+            feature.path,
+            'lib',
+            'src',
+            'presentation',
+            'l10n',
+            'arb',
+            '${feature.name}_$language.arb',
+          ),
+        );
+
+  final File _file;
+
+  final String language;
+
+  String get path => _file.path;
+
+  void delete() => _file.deleteSync(recursive: true);
+}
+
+/// Thrown when [L10nFile.templateArbFile] fails to read the `template-arb-file` property.
+class ReadTemplateArbFileFailure implements Exception {}
+
+/// {@template l10n_file}
+/// Abstraction of the l10n file of a feature of an existing Rapid project.
+/// {@endtemplate}
+class L10nFile {
+  /// {@macro l10n_file}
+  L10nFile({String path = '.'}) : _file = YamlFile(path: path, name: 'l10n');
+
+  /// The underlying yaml file.
+  final YamlFile _file;
+
+  String get path => _file.path;
+
+  /// The `template-arb-file` property.
+  String templateArbFile() {
+    try {
+      return _file.readValue(['template-arb-file']);
+    } catch (_) {
+      throw ReadTemplateArbFileFailure();
+    }
+  }
+}
---
+++ packages/rapid_cli/test/src/project/feature_test.dart
@@ -0,0 +1,309 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as p;
+import 'package:rapid_cli/src/core/platform.dart';
+import 'package:rapid_cli/src/project/feature.dart';
+import 'package:rapid_cli/src/project/melos_file.dart';
+import 'package:rapid_cli/src/project/platform_directory.dart';
+import 'package:rapid_cli/src/project/project.dart';
+import 'package:test/test.dart';
+import 'package:universal_io/io.dart';
+
+const l10nWithTemplateArbFile = '''
+template-arb-file: foo_bar
+''';
+
+const l10nWithoutTemplateArbFile = '''
+some: value
+''';
+
+class MockMelosFile extends Mock implements MelosFile {}
+
+class MockProject extends Mock implements Project {}
+
+class MockPlatformDirectory extends Mock implements PlatformDirectory {}
+
+class MockFeature extends Mock implements Feature {}
+
+void main() {
+  group('Feature', () {
+    final cwd = Directory.current;
+
+    const projectName = 'my_app';
+    late MelosFile melosFile;
+    late Project project;
+    const platform = Platform.android;
+    const platformDirPath = 'bom/bam';
+    late PlatformDirectory platformDirectory;
+    const featureName = 'my_feature';
+    late Feature feature;
+
+    setUp(() {
+      Directory.current = Directory.systemTemp.createTempSync().path;
+
+      melosFile = MockMelosFile();
+      when(() => melosFile.name()).thenReturn(projectName);
+      project = MockProject();
+      when(() => project.melosFile).thenReturn(melosFile);
+      platformDirectory = MockPlatformDirectory();
+      when(() => platformDirectory.platform).thenReturn(platform);
+      when(() => platformDirectory.path).thenReturn(platformDirPath);
+      when(() => platformDirectory.project).thenReturn(project);
+      feature = Feature(
+        name: featureName,
+        platformDirectory: platformDirectory,
+      );
+      Directory(feature.path).createSync(recursive: true);
+      Directory(
+        p.join(feature.path, 'lib', 'src', 'presentation', 'l10n', 'arb'),
+      ).createSync(recursive: true);
+    });
+
+    tearDown(() {
+      Directory.current = cwd;
+    });
+
+    group('delete', () {
+      test('deletes the directory', () {
+        // Arrange
+        final directory = Directory(feature.path);
+
+        // Act
+        feature.delete();
+
+        // Assert
+        expect(directory.existsSync(), false);
+      });
+    });
+
+    group('defaultLanguage', () {
+      test('returns the correct default language extracted from l10n.yaml', () {
+        // Arrange
+        final l10nFile = File(p.join(feature.path, 'l10n.yaml'));
+        l10nFile.createSync(recursive: true);
+        l10nFile.writeAsStringSync('template-arb-file: ${featureName}_fr.arb');
+
+        // Act
+        final defaultLanguage = feature.defaultLanguage();
+
+        // Assert
+        expect(defaultLanguage, 'fr');
+      });
+    });
+
+    group('exists', () {
+      test('returns true when the directory exists', () {
+        // Act
+        final exists = feature.exists();
+
+        // Assert
+        expect(exists, true);
+      });
+
+      test('returns false when the directory does not exists', () {
+        // Arrange
+        Directory(feature.path).deleteSync(recursive: true);
+
+        // Act
+        final exists = feature.exists();
+
+        // Assert
+        expect(exists, false);
+      });
+    });
+
+    group('findArbFileByLanguage', () {
+      test('returns the arb file corresponding to the language', () {
+        // Arrange
+        const language = 'de';
+        final path = p.join(
+          feature.path,
+          'lib',
+          'src',
+          'presentation',
+          'l10n',
+          'arb',
+          '${featureName}_$language.arb',
+        );
+        final file = File(path);
+        file.createSync(recursive: true);
+
+        // Act
+        final arbFile = feature.findArbFileByLanguage(language);
+
+        // Assert
+        expect(arbFile.path, path);
+        expect(arbFile.language, language);
+      });
+
+      test('throws when the arb file does not exist', () {
+        // Arrange
+        const language = 'de';
+
+        // Act & Assert
+        expect(
+          () => feature.findArbFileByLanguage(language),
+          throwsA(isA<ArbFileNotExisting>()),
+        );
+      });
+    });
+
+    group('supportsLanguage', () {
+      test('returns true when the corresponding .arb file exists', () {
+        // Arrange
+        const language = 'de';
+        final arbFile = File(
+          p.join(
+            feature.path,
+            'lib',
+            'src',
+            'presentation',
+            'l10n',
+            'arb',
+            '${featureName}_$language.arb',
+          ),
+        );
+        arbFile.createSync(recursive: true);
+
+        // Act
+        final supportsLanguage = feature.supportsLanguage(language);
+
+        // Assert
+        expect(supportsLanguage, true);
+      });
+
+      test('returns false when the corresponding .arb file does not exist', () {
+        // Arrange
+        const language = 'de';
+
+        // Act
+        final supportsLanguage = feature.supportsLanguage(language);
+
+        // Assert
+        expect(supportsLanguage, false);
+      });
+    });
+
+    group('name', () {
+      test('is correct', () {
+        // Assert
+        expect(feature.name, featureName);
+      });
+    });
+
+    group('path', () {
+      test('is correct', () {
+        // Assert
+        expect(
+          feature.path,
+          '$platformDirPath/${projectName}_${platform.name}_$featureName',
+        );
+      });
+    });
+
+    group('platform', () {
+      test('is correct', () {
+        // Assert
+        expect(feature.platform, platform);
+      });
+    });
+  });
+
+  group('ArbFile', () {
+    final cwd = Directory.current;
+
+    const featureName = 'my_feature';
+    const featurePath = 'foo/bar';
+    late Feature feature;
+    const language = 'en';
+    late ArbFile arbFile;
+
+    setUp(() {
+      Directory.current = Directory.systemTemp.createTempSync().path;
+
+      feature = MockFeature();
+      when(() => feature.name).thenReturn(featureName);
+      when(() => feature.path).thenReturn(featurePath);
+      arbFile = ArbFile(language: language, feature: feature);
+      File(arbFile.path).createSync(recursive: true);
+    });
+
+    tearDown(() {
+      Directory.current = cwd;
+    });
+
+    group('language', () {
+      test('is correct', () {
+        // Assert
+        expect(arbFile.language, language);
+      });
+    });
+
+    group('path', () {
+      test('is correct', () {
+        // Assert
+        expect(
+          arbFile.path,
+          'foo/bar/lib/src/presentation/l10n/arb/my_feature_en.arb',
+        );
+      });
+    });
+
+    group('delete', () {
+      test('deletes the file', () {
+        // Arrange
+        final file = File(arbFile.path);
+
+        // Act
+        arbFile.delete();
+
+        // Assert
+        expect(file.existsSync(), false);
+      });
+    });
+  });
+
+  group('L10nFile', () {
+    final cwd = Directory.current;
+
+    late L10nFile l10nFile;
+
+    setUp(() {
+      Directory.current = Directory.systemTemp.createTempSync().path;
+
+      l10nFile = L10nFile();
+      File(l10nFile.path).createSync(recursive: true);
+    });
+
+    tearDown(() {
+      Directory.current = cwd;
+    });
+
+    group('path', () {
+      test('is correct', () {
+        // Assert
+        expect(l10nFile.path, 'l10n.yaml');
+      });
+    });
+
+    group('templateArbFile', () {
+      test('returns template-arb-file', () {
+        // Arrange
+        final file = File(l10nFile.path);
+        file.writeAsStringSync(l10nWithTemplateArbFile);
+
+        // Act + Assert
+        expect(l10nFile.templateArbFile(), 'foo_bar');
+      });
+
+      test('throws when name is not present', () {
+        // Arrange
+        final file = File(l10nFile.path);
+        file.writeAsStringSync(l10nWithoutTemplateArbFile);
+
+        // Act + Assert
+        expect(() => l10nFile.templateArbFile(),
+            throwsA(isA<ReadTemplateArbFileFailure>()));
+      });
+    });
+  });
+}

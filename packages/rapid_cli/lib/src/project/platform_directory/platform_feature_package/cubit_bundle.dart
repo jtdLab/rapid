cf1c6aa347f5e1d19854b474c639762c589c0ba7
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-01-23 09:54:04 +0100

    refactor: cleanup

--- packages/rapid_cli/lib/src/project/platform_directory/platform_feature_package/cubit_bundle.dart
+++ packages/rapid_cli/lib/src/project/platform_directory/platform_feature_package/cubit_bundle.dart
@@ -3,4 +3,48 @@
 
 import 'package:mason/mason.dart';
 
-final cubitBundle = MasonBundle.fromJson(<String, dynamic>{"files":[{"path":"lib/src/application/{{name.snakeCase()}}/{{name.snakeCase()}}_cubit.dart","data":"aW1wb3J0ICdwYWNrYWdlOmJsb2MvYmxvYy5kYXJ0JzsKaW1wb3J0ICdwYWNrYWdlOmZyZWV6ZWRfYW5ub3RhdGlvbi9mcmVlemVkX2Fubm90YXRpb24uZGFydCc7CmltcG9ydCAncGFja2FnZTppbmplY3RhYmxlL2luamVjdGFibGUuZGFydCc7CmltcG9ydCAncGFja2FnZTpyYXBpZF9kaS9yYXBpZF9kaS5kYXJ0JzsKCnBhcnQgJ3t7bmFtZS5zbmFrZUNhc2UoKX19X2N1Yml0LmZyZWV6ZWQuZGFydCc7CnBhcnQgJ3t7bmFtZS5zbmFrZUNhc2UoKX19X3N0YXRlLmRhcnQnOwoKQHt7cGxhdGZvcm19fQpAaW5qZWN0YWJsZQpjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1DdWJpdCBleHRlbmRzIEN1Yml0PHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlPiB7CiAge3tuYW1lLnBhc2NhbENhc2UoKX19Q3ViaXQoKQogICAgICA6IHN1cGVyKAogICAgICAgICAgLy8gU2V0IGluaXRpYWwgc3RhdGUKICAgICAgICAgIGNvbnN0IHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlLmluaXRpYWwoKSwKICAgICAgICApOwoKICB2b2lkIHN0YXJ0ZWQoKSB7CiAgICAvLyBUT0RPOiBpbXBsZW1lbnQKICAgIHRocm93IFVuaW1wbGVtZW50ZWRFcnJvcigpOwogIH0KfQo=","type":"text"},{"path":"lib/src/application/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart","data":"cGFydCBvZiAne3tuYW1lLnNuYWtlQ2FzZSgpfX1fY3ViaXQuZGFydCc7CgpAZnJlZXplZApjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSB3aXRoIF8ke3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUgewogIGNvbnN0IGZhY3Rvcnkge3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUuaW5pdGlhbCgpID0gX0luaXRpYWw7Cn0K","type":"text"}],"hooks":[],"name":"cubit","description":"A brick generating a cubit of a platform feature package of a Rapid project.","version":"0.1.0+1","environment":{"mason":">=0.1.0-dev.39 <0.1.0"},"readme":{"path":"README.md","data":"IyBjdWJpdAoKWyFbUG93ZXJlZCBieSBNYXNvbl0oaHR0cHM6Ly9pbWcuc2hpZWxkcy5pby9lbmRwb2ludD91cmw9aHR0cHMlM0ElMkYlMkZ0aW55dXJsLmNvbSUyRm1hc29uLWJhZGdlKV0oaHR0cHM6Ly9naXRodWIuY29tL2ZlbGFuZ2VsL21hc29uKQoKQSBuZXcgYnJpY2sgY3JlYXRlZCB3aXRoIHRoZSBNYXNvbiBDTEkuCgpfR2VuZXJhdGVkIGJ5IFttYXNvbl1bMV0g8J+nsV8KCiMjIEdldHRpbmcgU3RhcnRlZCDwn5qACgpUaGlzIGlzIGEgc3RhcnRpbmcgcG9pbnQgZm9yIGEgbmV3IGJyaWNrLgpBIGZldyByZXNvdXJjZXMgdG8gZ2V0IHlvdSBzdGFydGVkIGlmIHRoaXMgaXMgeW91ciBmaXJzdCBicmljayB0ZW1wbGF0ZToKCi0gW09mZmljaWFsIE1hc29uIERvY3VtZW50YXRpb25dWzJdCi0gW0NvZGUgZ2VuZXJhdGlvbiB3aXRoIE1hc29uIEJsb2ddWzNdCi0gW1ZlcnkgR29vZCBMaXZlc3RyZWFtOiBGZWxpeCBBbmdlbG92IERlbW9zIE1hc29uXVs0XQoKWzFdOiBodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvbWFzb24KWzJdOiBodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvbWFzb24vdHJlZS9tYXN0ZXIvcGFja2FnZXMvbWFzb25fY2xpI3JlYWRtZQpbM106IGh0dHBzOi8vdmVyeWdvb2QudmVudHVyZXMvYmxvZy9jb2RlLWdlbmVyYXRpb24td2l0aC1tYXNvbgpbNF06IGh0dHBzOi8veW91dHUuYmUvRzRQVGpBNnRwVFUK","type":"text"},"changelog":{"path":"CHANGELOG.md","data":"IyAwLjEuMCsxCgotIFRPRE86IERlc2NyaWJlIGluaXRpYWwgcmVsZWFzZS4K","type":"text"},"license":{"path":"LICENSE","data":"VE9ETzogQWRkIHlvdXIgbGljZW5zZSBoZXJlLgo=","type":"text"},"vars":{"project_name":{"type":"string","description":"The project name"},"name":{"type":"string","description":"The name of the bloc"},"platform":{"type":"string","description":"The platform of the feature"}}});
+final cubitBundle = MasonBundle.fromJson(<String, dynamic>{
+  "files": [
+    {
+      "path":
+          "lib/src/application/{{name.snakeCase()}}/{{name.snakeCase()}}_cubit.dart",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmJsb2MvYmxvYy5kYXJ0JzsKaW1wb3J0ICdwYWNrYWdlOmZyZWV6ZWRfYW5ub3RhdGlvbi9mcmVlemVkX2Fubm90YXRpb24uZGFydCc7CmltcG9ydCAncGFja2FnZTppbmplY3RhYmxlL2luamVjdGFibGUuZGFydCc7CmltcG9ydCAncGFja2FnZTpyYXBpZF9kaS9yYXBpZF9kaS5kYXJ0JzsKCnBhcnQgJ3t7bmFtZS5zbmFrZUNhc2UoKX19X2N1Yml0LmZyZWV6ZWQuZGFydCc7CnBhcnQgJ3t7bmFtZS5zbmFrZUNhc2UoKX19X3N0YXRlLmRhcnQnOwoKQHt7cGxhdGZvcm19fQpAaW5qZWN0YWJsZQpjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1DdWJpdCBleHRlbmRzIEN1Yml0PHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlPiB7CiAge3tuYW1lLnBhc2NhbENhc2UoKX19Q3ViaXQoKQogICAgICA6IHN1cGVyKAogICAgICAgICAgLy8gU2V0IGluaXRpYWwgc3RhdGUKICAgICAgICAgIGNvbnN0IHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlLmluaXRpYWwoKSwKICAgICAgICApOwoKICB2b2lkIHN0YXJ0ZWQoKSB7CiAgICAvLyBUT0RPOiBpbXBsZW1lbnQKICAgIHRocm93IFVuaW1wbGVtZW50ZWRFcnJvcigpOwogIH0KfQo=",
+      "type": "text"
+    },
+    {
+      "path":
+          "lib/src/application/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart",
+      "data":
+          "cGFydCBvZiAne3tuYW1lLnNuYWtlQ2FzZSgpfX1fY3ViaXQuZGFydCc7CgpAZnJlZXplZApjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSB3aXRoIF8ke3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUgewogIGNvbnN0IGZhY3Rvcnkge3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUuaW5pdGlhbCgpID0gX0luaXRpYWw7Cn0K",
+      "type": "text"
+    }
+  ],
+  "hooks": [],
+  "name": "cubit",
+  "description":
+      "A brick generating a cubit of a platform feature package of a Rapid project.",
+  "version": "0.1.0+1",
+  "environment": {"mason": ">=0.1.0-dev.39 <0.1.0"},
+  "readme": {
+    "path": "README.md",
+    "data":
+        "IyBjdWJpdAoKWyFbUG93ZXJlZCBieSBNYXNvbl0oaHR0cHM6Ly9pbWcuc2hpZWxkcy5pby9lbmRwb2ludD91cmw9aHR0cHMlM0ElMkYlMkZ0aW55dXJsLmNvbSUyRm1hc29uLWJhZGdlKV0oaHR0cHM6Ly9naXRodWIuY29tL2ZlbGFuZ2VsL21hc29uKQoKQSBuZXcgYnJpY2sgY3JlYXRlZCB3aXRoIHRoZSBNYXNvbiBDTEkuCgpfR2VuZXJhdGVkIGJ5IFttYXNvbl1bMV0g8J+nsV8KCiMjIEdldHRpbmcgU3RhcnRlZCDwn5qACgpUaGlzIGlzIGEgc3RhcnRpbmcgcG9pbnQgZm9yIGEgbmV3IGJyaWNrLgpBIGZldyByZXNvdXJjZXMgdG8gZ2V0IHlvdSBzdGFydGVkIGlmIHRoaXMgaXMgeW91ciBmaXJzdCBicmljayB0ZW1wbGF0ZToKCi0gW09mZmljaWFsIE1hc29uIERvY3VtZW50YXRpb25dWzJdCi0gW0NvZGUgZ2VuZXJhdGlvbiB3aXRoIE1hc29uIEJsb2ddWzNdCi0gW1ZlcnkgR29vZCBMaXZlc3RyZWFtOiBGZWxpeCBBbmdlbG92IERlbW9zIE1hc29uXVs0XQoKWzFdOiBodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvbWFzb24KWzJdOiBodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvbWFzb24vdHJlZS9tYXN0ZXIvcGFja2FnZXMvbWFzb25fY2xpI3JlYWRtZQpbM106IGh0dHBzOi8vdmVyeWdvb2QudmVudHVyZXMvYmxvZy9jb2RlLWdlbmVyYXRpb24td2l0aC1tYXNvbgpbNF06IGh0dHBzOi8veW91dHUuYmUvRzRQVGpBNnRwVFUK",
+    "type": "text"
+  },
+  "changelog": {
+    "path": "CHANGELOG.md",
+    "data": "IyAwLjEuMCsxCgotIFRPRE86IERlc2NyaWJlIGluaXRpYWwgcmVsZWFzZS4K",
+    "type": "text"
+  },
+  "license": {
+    "path": "LICENSE",
+    "data": "VE9ETzogQWRkIHlvdXIgbGljZW5zZSBoZXJlLgo=",
+    "type": "text"
+  },
+  "vars": {
+    "project_name": {"type": "string", "description": "The project name"},
+    "name": {"type": "string", "description": "The name of the bloc"},
+    "platform": {"type": "string", "description": "The platform of the feature"}
+  }
+});

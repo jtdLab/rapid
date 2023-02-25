e31d874367178c028fb11c3fc6f2781928e2ff9a
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-02-25 15:26:33 +0100

    fix: rm rapid package from repo and templates

--- packages/rapid/rapid/rapid/lib/rapid.dart
+++
@@ -1,6 +0,0 @@
-/// Package used in the app package of a Rapid project.
-///
-/// This contains code required in the setup and bootstrap process.
-library rapid;
-
-export 'src/run_on_platform.dart';

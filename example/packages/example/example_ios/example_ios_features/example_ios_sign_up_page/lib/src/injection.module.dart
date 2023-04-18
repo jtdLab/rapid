f2d89bb5ba9a3bda32518e244b238cd12a11a45a
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-04-19 00:02:14 +0200

    feat(example): implement navigation package ios

--- example/packages/example/example_ios/example_ios_features/example_ios_sign_up_page/lib/src/injection.module.dart
+++ example/packages/example/example_ios/example_ios_features/example_ios_sign_up_page/lib/src/injection.module.dart
@@ -1,15 +1,26 @@
-// coverage:ignore-file
 //@GeneratedMicroModule;ExampleIosSignUpPagePackageModule;package:example_ios_sign_up_page/src/injection.module.dart
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unnecessary_lambdas
+// ignore_for_file: lines_longer_than_80_chars
+// coverage:ignore-file
+
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i2;
 
+import 'package:example_ios_navigation/example_ios_navigation.dart' as _i3;
+import 'package:example_ios_sign_up_page/src/presentation/navigator.dart'
+    as _i4;
 import 'package:injectable/injectable.dart' as _i1;
 
-// ignore_for_file: unnecessary_lambdas
-// ignore_for_file: lines_longer_than_80_chars
+const String _ios = 'ios';
+
 class ExampleIosSignUpPagePackageModule extends _i1.MicroPackageModule {
-  // initializes the registration of main-scope dependencies inside of [GetIt]
+  // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i2.FutureOr<void> init(_i1.GetItHelper gh) {}
+  _i2.FutureOr<void> init(_i1.GetItHelper gh) {
+    gh.lazySingleton<_i3.ISignUpPageNavigator>(
+      () => _i4.SignUpPageNavigator(),
+      registerFor: {_ios},
+    );
+  }
 }
--- example/packages/example/example_ios/example_ios_navigation/lib/example_ios_navigation.dart
+++ example/packages/example/example_ios/example_ios_navigation/lib/example_ios_navigation.dart
@@ -3,4 +3,5 @@
 /// To use, import `package:example_ios_navigation/example_ios_navigation.dart`.
 library example_ios_navigation;
 
-export 'src/i_home_page_navigator.dart';
+export 'src/i_protected_router_navigator.dart';
+export 'src/i_sign_up_page_navigator.dart';

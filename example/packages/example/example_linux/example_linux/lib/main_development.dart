b60e6a49d83c1100d0ee19627b4a7e7335f39fb7
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-04-18 18:43:35 +0200

    ci: more ci

--- example/packages/example/example_linux/example_linux/lib/main_development.dart
+++ example/packages/example/example_linux/example_linux/lib/main_development.dart
@@ -11,5 +11,6 @@ void main() => runOnLinux(() async {
       WidgetsFlutterBinding.ensureInitialized();
       // TODO: add Linux development setup here (post WidgetsFlutterBinding.ensureInitialized())
 
+
       await bootstrap();
     });

c0bba358635e8b7cdfe1399dfd6653c7c3c83562
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-04-18 22:00:30 +0200

    feat(example): implement root package ios

--- example/packages/example/example_ios/example_ios/lib/router.dart
+++ example/packages/example/example_ios/example_ios/lib/router.dart
@@ -1,19 +1,23 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:example_ios_home_page/example_ios_home_page.dart';
+import 'package:example_ios_protected_router/example_ios_protected_router.dart';
+import 'package:example_ios_public_router/example_ios_public_router.dart';
 
 /// Setup auto route which generates routing code.
 ///
 /// For more info see: https://pub.dev/packages/auto_route
 class Router extends RootStackRouter {
-  final _homePageRouter = HomePageRouter();
+  final _publicRouter = PublicRouter();
+  final _protectedRouter = ProtectedRouter();
 
   @override
   Map<String, PageFactory> get pagesMap => {
-        ..._homePageRouter.pagesMap,
+        ..._publicRouter.pagesMap,
+        ..._protectedRouter.pagesMap,
       };
 
   @override
   List<AutoRoute> get routes => [
-        ..._homePageRouter.routes,
+        ..._publicRouter.routes,
+        ..._protectedRouter.routes,
       ];
 }

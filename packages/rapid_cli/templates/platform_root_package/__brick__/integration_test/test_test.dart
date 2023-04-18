7bb5838c5fc0edc706e731b87a7022314082a7f2
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-04-18 04:38:34 +0200

    refactor: update e2e tests in root package

--- packages/rapid_cli/templates/platform_root_package/__brick__/integration_test/test_test.dart
+++ packages/rapid_cli/templates/platform_root_package/__brick__/integration_test/test_test.dart
@@ -2,14 +2,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:{{project_name}}_{{#android}}android{{/android}}{{#ios}}ios{{/ios}}{{#linux}}linux{{/linux}}{{#macos}}macos{{/macos}}{{#web}}web{{/web}}{{#windows}}windows{{/windows}}/main_test.dart'
     as test;
+import 'package:{{project_name}}_di/{{project_name}}_di.dart';
+
+import 'start_app.dart' as start_app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  setUp(() async {
+    await getIt.reset();
+  });
+
   group('E2E (test)', () {
-    testWidgets('runs successfully', (tester) async {
+    testWidgets('start app', (tester) async {
       test.main();
-      await tester.pumpAndSettle();
+
+      await start_app.performTest(tester);
     });
   });
 }

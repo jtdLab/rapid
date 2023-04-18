e9f3df0d55e7876e97a9f3880a529f46f95b9893
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-04-18 15:16:39 +0200

    test: rm fixtures

--- packages/rapid_cli/test/e2e/fixtures/project_web/packages/project_web/project_web_di/test/src/container_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test/test.dart';
-import 'package:project_web_di/project_web_di.dart';
-
-void main() {
-  test('getIt', () {
-    expect(getIt, isNotNull);
-  });
-}
--- packages/rapid_cli/test/e2e/fixtures/project_windows/packages/project_windows/project_windows_logging/lib/project_windows_logging.dart
+++
@@ -1,7 +0,0 @@
-/// Logging for Project Windows.
-///
-/// To use, import `package:project_windows_logging/project_windows_logging.dart`.
-library project_windows_logging;
-
-export 'src/injection.module.dart';
-export 'src/logger.dart';

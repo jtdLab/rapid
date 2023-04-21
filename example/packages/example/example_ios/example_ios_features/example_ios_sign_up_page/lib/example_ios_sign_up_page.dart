c452332003fa821494903bf6faaf3edf3e8d6d11
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-04-21 16:31:55 +0200

    feat(example): add apllication layer to barrel file

--- example/packages/example/example_ios/example_ios_features/example_ios_sign_up_page/lib/example_ios_sign_up_page.dart
+++ example/packages/example/example_ios/example_ios_features/example_ios_sign_up_page/lib/example_ios_sign_up_page.dart
@@ -3,5 +3,6 @@
 /// To use, import `package:example_ios_sign_up_page/example_ios_sign_up_page.dart`.
 library example_ios_sign_up_page;
 
+export 'src/application/application.dart';
 export 'src/injection.module.dart';
 export 'src/presentation/presentation.dart';

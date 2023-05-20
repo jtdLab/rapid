66cc77caffc5a22a1feb121dbd89b2a140c80560
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-05-21 01:52:08 +0200

    fix: feature package template has wrong barrel file export

--- packages/rapid_cli/templates/platform_feature_package/__brick__/lib/src/presentation/presentation.dart
+++ packages/rapid_cli/templates/platform_feature_package/__brick__/lib/src/presentation/presentation.dart
@@ -1,30 +1,30 @@
 {{#android}}export '{{name}}.dart';
 export 'l10n/{{project_name}}_android_{{name}}_localizations.dart';
-{{#routable}}export 'router.dart';{{/routable}}
+{{#routable}}export 'routes.dart';{{/routable}}
 {{#routable}}export 'routes.module.dart';{{/routable}}
 {{/android}}{{#ios}}
 export '{{name}}.dart';
 export 'l10n/{{project_name}}_ios_{{name}}_localizations.dart';
-{{#routable}}export 'router.dart';{{/routable}}
+{{#routable}}export 'routes.dart';{{/routable}}
 {{#routable}}export 'routes.module.dart';{{/routable}}
 {{/ios}}{{#linux}}
 export '{{name}}.dart';
 export 'l10n/{{project_name}}_linux_{{name}}_localizations.dart';
-{{#routable}}export 'router.dart';{{/routable}}
+{{#routable}}export 'routes.dart';{{/routable}}
 {{#routable}}export 'routes.module.dart';{{/routable}}
 {{/linux}}{{#macos}}
 export '{{name}}.dart';
 export 'l10n/{{project_name}}_macos_{{name}}_localizations.dart';
-{{#routable}}export 'router.dart';{{/routable}}
+{{#routable}}export 'routes.dart';{{/routable}}
 {{#routable}}export 'routes.module.dart';{{/routable}}
 {{/macos}}{{#web}}
 export '{{name}}.dart';
 export 'l10n/{{project_name}}_web_{{name}}_localizations.dart';
-{{#routable}}export 'router.dart';{{/routable}}
+{{#routable}}export 'routes.dart';{{/routable}}
 {{#routable}}export 'routes.module.dart';{{/routable}}
 {{/web}}{{#windows}}
 export '{{name}}.dart';
 export 'l10n/{{project_name}}_windows_{{name}}_localizations.dart';
-{{#routable}}export 'router.dart';{{/routable}}
+{{#routable}}export 'routes.dart';{{/routable}}
 {{#routable}}export 'routes.module.dart';{{/routable}}
 {{/windows}}

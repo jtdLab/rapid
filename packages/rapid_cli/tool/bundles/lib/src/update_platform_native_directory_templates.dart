0860fe66b44a74e54d6425069be7ed96260f338c
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-06-14 00:23:58 +0200

    fix: macos_ui workaround

--- packages/rapid_cli/tool/bundles/lib/src/update_platform_native_directory_templates.dart
+++ packages/rapid_cli/tool/bundles/lib/src/update_platform_native_directory_templates.dart
@@ -131,6 +131,7 @@ void _updatePlatformNativeDirectoryTemplate({
     );
   }
 
+  // TODO: https://github.com/jtdLab/rapid/issues/96
   if (platform == 'macos') {
     for (final file in brickDir
         .listSync(recursive: true)

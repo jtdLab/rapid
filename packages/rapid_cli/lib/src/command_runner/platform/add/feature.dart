0dc52a1fffb8ebd5a0cb00bdc673165a902a3f17
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-06-23 22:56:08 +0200

    chore: hide add custom feature command

--- packages/rapid_cli/lib/src/command_runner/platform/add/feature.dart
+++ packages/rapid_cli/lib/src/command_runner/platform/add/feature.dart
@@ -2,14 +2,13 @@ import 'package:rapid_cli/src/command_runner/util/platform_x.dart';
 import 'package:rapid_cli/src/core/platform.dart';
 
 import '../../base.dart';
-import 'feature/custom.dart';
 import 'feature/flow.dart';
 import 'feature/page.dart';
 import 'feature/widget.dart';
 
 class PlatformAddFeatureCommand extends RapidBranchCommand {
   PlatformAddFeatureCommand(this.platform, super.project) {
-    addSubcommand(PlatformAddFeatureCustomCommand(platform, project));
+    //addSubcommand(PlatformAddFeatureCustomCommand(platform, project));
     addSubcommand(PlatformAddFeatureFlowCommand(platform, project));
     addSubcommand(PlatformAddFeaturePageCommand(platform, project));
     addSubcommand(PlatformAddFeatureWidgetCommand(platform, project));

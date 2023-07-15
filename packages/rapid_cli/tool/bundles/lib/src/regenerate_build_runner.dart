5bec1681ef17d816f24d4608a9d132e01bc54431
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-07-15 04:17:01 +0200

    chore: update ci to exclude templates dir from formatting

--- packages/rapid_cli/tool/bundles/lib/src/regenerate_build_runner.dart
+++ packages/rapid_cli/tool/bundles/lib/src/regenerate_build_runner.dart
@@ -1,5 +1,5 @@
 // TODO mason make templates with placeholder vars
 // take output and place in a project with build_runner dev dep and other codegen stuff setup
-// run code gen then take generated files replace placeholder vars with template vars and move the 
+// run code gen then take generated files replace placeholder vars with template vars and move the
 // the result into the repsective template.
 // This way the generated files are always up to data to latest generator changes

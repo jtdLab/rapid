484eefa0b3dbefedfe456fac319915a2c98ae227
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-08-02 21:06:03 +0200

    fix: update value object template

--- packages/rapid_cli/templates/value_object/__brick__/test/src/{{{output_dir}}}/{{name.snakeCase()}}_test.dart
+++ packages/rapid_cli/templates/value_object/__brick__/test/src/{{{output_dir}}}/{{name.snakeCase()}}_test.dart
@@ -1,68 +1,89 @@
-import 'package:test/test.dart';
-import 'package:{{project_name}}_domain{{#has_sub_domain_name}}_{{sub_domain_name}}{{/has_sub_domain_name}}/src{{^output_dir_is_cwd}}/{{{output_dir}}}{{/output_dir_is_cwd}}/{{name.snakeCase()}}.dart';
+// import 'package:test/test.dart';
+// import 'package:{{project_name}}_domain{{#has_sub_domain_name}}_{{sub_domain_name}}{{/has_sub_domain_name}}/src{{^output_dir_is_cwd}}/{{{output_dir}}}{{/output_dir_is_cwd}}/{{name.snakeCase()}}.dart';
 
 void main() {
-  group('{{name.pascalCase()}}', () {
+/*   group('{{name.pascalCase()}}', () {
     group('.()', () {
-      group('(valid)', () {
-        test('', () {
-          // Arrange + Act
-          // final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO valid */);
+      test(/* TODO: valid */, () {
+        // Arrange + Act
+        final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO: valid */);
 
-          // Assert
-          // expect({{name.camelCase()}}.value, right(/* TODO valid */));
-        });
-
-        // TODO: more valid cases
+        // Assert
+        expect(
+          {{name.camelCase()}},
+          isA<Valid{{name.pascalCase()}}>().having(
+            (e) => e.value,
+            'value',
+            /* TODO: valid */,
+          ),
+        );
       });
 
-      group('(invalid)', () {
-        test('', () {
-          // Arrange + Act
-          // final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO invalid */);
+      // TODO: more valid cases (optional)
 
-          // Assert
-          // expect({{name.camelCase()}}.value, left(/* TODO {{name.pascalCase()}}Failure */));
-        });
+      test(/* TODO: invalid */, () {
+        // Arrange + Act
+        final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO: invalid */);
 
-        // TODO: more invalid cases
+        // Assert
+        expect({{name.camelCase()}}, isA<{{name.pascalCase()}}FailureFoo>());
       });
+
+      // TODO: more invalid cases (optional)
     });
 
     group('.random()', () {
-      test('(valid)', () {
+      test('creates valid instance', () {
         // Arrange + Act
-        // final {{name.camelCase()}} = {{name.pascalCase()}}.random(isValid: true);
+        final {{name.camelCase()}} = {{name.pascalCase()}}.random(valid: true);
 
         // Assert
-        // expect({{name.camelCase()}}.value, right(/* TODO valid */));
+        expect({{name.camelCase()}}, isA<Valid{{name.pascalCase()}}>());
       });
 
-      test('(invalid)', () {
+      test('creates invalid instance', () {
         // Arrange + Act
-        // final {{name.camelCase()}} = {{name.pascalCase()}}.random(isValid: false);
+        final {{name.camelCase()}} = {{name.pascalCase()}}.random(valid: false);
 
         // Assert
-        // expect({{name.camelCase()}}.value, left(/* TODO {{name.pascalCase()}}Failure */));
+        expect({{name.camelCase()}}, isA<{{name.pascalCase()}}FailureFoo>());
+      });
+    });
+
+    group('getOrCrash', () {
+      test('returns value when valid', () {
+        // Arrange + Act
+        final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO: valid */);
+
+        // Assert
+        expect({{name.camelCase()}}.getOrCrash(), /* TODO: valid */);
+      });
+
+      test('throws StateError when invalid', () {
+        // Arrange + Act
+        final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO: invalid */);
+
+        // Assert
+        expect({{name.camelCase()}}.getOrCrash(), throwsStateError);
       });
     });
 
-    group('.toString()', () {
-      test('(valid)', () {
+    group('isValid', () {
+      test('returns true when valid', () {
         // Arrange + Act
-        // final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO valid */);
+        final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO: valid */);
 
         // Assert
-        // expect({{name.camelCase()}}.toString(), /* TODO */);
+        expect({{name.camelCase()}}.isValid(), true);
       });
 
-      test('(invalid)', () {
+      test('returns false when invalid', () {
         // Arrange + Act
-        // final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO invalid */);
+        final {{name.camelCase()}} = {{name.pascalCase()}}(/* TODO: invalid */);
 
         // Assert
-        // expect({{name.camelCase()}}.toString(), /* TODO */);
+        expect({{name.camelCase()}}.isValid(), false);
       });
     });
-  });
+  }); */
 }
